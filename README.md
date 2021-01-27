210bfd77018ce9f98b7160ec96af080208172524
Luciano Ropero <lropero@gmail.com>
2021-01-27 19:20:01 -0300

    Enhancements

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Portfolio tracker
 
 Crypto portfolio tracker.
+
+<img src="tracker.png?raw=true" width="380">
+
+### Requires
+
+- Node v14.15.4
+
+### Installation
+
+```sh
+$ npm ci
+```
+
+### Configuration
+
+- Create API key at [CoinMarketCap](https://coinmarketcap.com/api/)
+- Edit `.env` file with your key, time delay between polls (in minutes) and your holdings:
+
+```sh
+APIKEY=<YOUR_API_KEY>
+DELAY=5
+PORTFOLIO={"BTC":0.1234,"ETH":1.2}
+```
+
+### Usage
+
+```sh
+$ npm run start
+```
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2021, Luciano Ropero <lropero@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 const chalk = require('chalk')
 const CoinMarketCap = require('coinmarketcap-api')
 const dotenv = require('dotenv')
+const logUpdate = require('log-update')
 const { arrowRight } = require('figures')
+const { delay, mergeMap, repeat, tap } = require('rxjs/operators')
+const { of } = require('rxjs')
 
-const portfolio = require('./portfolio')
+const { version } = require('./package.json')
 
 dotenv.config()
 
-const run = async () => {
-  const client = new CoinMarketCap(process.env.APIKEY)
-  const symbols = Object.keys(portfolio)
-  const { data } = await client.getQuotes({ symbol: symbols })
-  const quotes = symbols.reduce((quotes, symbol) => {
-    quotes[symbol] = data[symbol].quote.USD.price
-    return quotes
-  }, {})
+const client = new CoinMarketCap(process.env.APIKEY)
+const portfolio = JSON.parse(process.env.PORTFOLIO)
+const symbols = Object.keys(portfolio)
+
+let previousQuotes
+let previousTotal
+
+const display = quotes => {
   const values = symbols.reduce((values, symbol) => {
     values[symbol] = portfolio[symbol] * quotes[symbol]
     return values
@@ -23,28 +43,47 @@ const run = async () => {
     .sort((a, b) => b[1] - a[1])
     .map(entry => entry[0])
     .reduce((valuesFormatted, symbol) => {
-      valuesFormatted[symbol] = new Intl.NumberFormat('es-AR', {
-        currency: 'USD',
-        style: 'currency'
-      }).format(values[symbol])
+      valuesFormatted[symbol] = chalk[
+        previousQuotes ? (previousQuotes[symbol] > quotes[symbol] ? 'red' : previousQuotes[symbol] < quotes[symbol] ? 'green' : 'blue') : 'blue'
+      ](
+        new Intl.NumberFormat('en-US', {
+          currency: 'USD',
+          style: 'currency'
+        }).format(values[symbol])
+      )
       return valuesFormatted
     }, {})
-  Object.keys(valuesFormatted).forEach(symbol => {
-    console.log(
-      `${chalk.yellow(symbol)} ${arrowRight} ${chalk.green(
-        valuesFormatted[symbol]
-      )}`
-    )
-  })
   const total = Object.values(values).reduce((total, value) => total + value, 0)
-  console.log(
-    chalk.blue(
-      new Intl.NumberFormat('es-AR', {
+  logUpdate(
+    `${Object.keys(valuesFormatted)
+      .map(symbol => `${chalk.yellow(symbol)} ${arrowRight} ${valuesFormatted[symbol]}`)
+      .join('\n')}\n${chalk.cyan('TOTAL')} ${chalk[previousTotal ? (previousTotal > total ? 'red' : previousTotal < total ? 'green' : 'blue') : 'blue'](
+      new Intl.NumberFormat('en-US', {
         currency: 'USD',
         style: 'currency'
       }).format(total)
-    )
+    )}`
   )
+  previousQuotes = quotes
+  previousTotal = total
 }
 
-run()
+const tracker = of({}).pipe(
+  mergeMap(_ => updateQuotes()),
+  tap(display),
+  delay(1000 * 60 * process.env.DELAY),
+  repeat()
+)
+
+const updateQuotes = async () => {
+  const { data } = await client.getQuotes({ symbol: symbols })
+  const quotes = symbols.reduce((quotes, symbol) => {
+    quotes[symbol] = data[symbol].quote.USD.price
+    return quotes
+  }, {})
+  return quotes
+}
+
+console.log(chalk.green(`Portfolio tracker v${version}`))
+console.log(chalk.gray(`Like it? Share the love :) ${arrowRight} 1B7owVfYhLjWLh9NWivQAKJHBcf8Doq54i (BTC)\n`))
+tracker.subscribe()
